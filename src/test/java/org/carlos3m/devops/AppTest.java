21650f746bf2de06f711c796c97a70eba9ac7f9c
pacosoft2000 <pacosoft2000@yahoo.es>
2022-11-20 22:56:38 +0100

    Add files via upload

---
+++ src/test/java/org/carlos3m/devops/AppTest.java
@@ -0,0 +1,20 @@
+package org.carlos3m.devops;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
